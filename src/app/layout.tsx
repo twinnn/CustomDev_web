4a3e66cd318d7a941963eb8a0144685f63c3c4c7
Peter Pojtek <peter.pojtek@thermofisher.com>
2026-02-23 18:29:29 +0100

    add ahref web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "./components/navbar";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -8,6 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sk" className="font-silka-500">
+      <head>
+        <Script src="https://analytics.ahrefs.com/analytics.js" data-key="YrhlWQ8ukCVsUeJNodQjlA" async />
+      </head>
       <body>
         <div className="fixed top-0 left-0 w-full h-16 z-50 bg-[#0D1128]/15 backdrop-blur-sm">
           <Navbar />
